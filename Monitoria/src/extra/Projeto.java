52da77962689989f59c57d90c8788f9b1a33c27a
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2014-12-04 14:02:37 -1200

    Adicionado o get

--- Monitoria/src/extra/Projeto.java
+++ Monitoria/src/extra/Projeto.java
@@ -3,11 +3,12 @@ package extra;
 public class Projeto {
 	private String proj;
 	
-	//removido
-	
+
 	public void setProj(String newProj){
 		 proj = newProj;
 	}
 	
-	
+	public String getProj(){
+		return proj;
+	}
 }
