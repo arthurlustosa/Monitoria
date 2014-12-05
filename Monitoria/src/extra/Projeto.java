073efa033c79cb473f3ba5c3f28e9dc5cc6801c0
arthurlustosa <arthur.souza@ccc.ufcg.edu.br>
2014-12-04 13:49:28 -1200

    metodo get removido

--- Monitoria/src/extra/Projeto.java
+++ Monitoria/src/extra/Projeto.java
@@ -3,13 +3,11 @@ package extra;
 public class Projeto {
 	private String proj;
 	
+	//removido
+	
 	public void setProj(String newProj){
 		 proj = newProj;
 	}
 	
-	public String getProjt(){
-		return proj;
-	}
 	
-
 }
